80a7dfda0f91d2c8051e6f1dde30b289c1628c06
Xdd1024 <951672513@qq.com>
2021-11-10 11:11:35 +0800

    第一次提交

---
+++ src/main/java/org/commit/file/Test.java
@@ -0,0 +1,13 @@
+package org.commit.file;
+
+/**
+ * 文件说明：
+ *
+ * @author XuBin
+ * @createDT 2021/11/10 11:07
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
